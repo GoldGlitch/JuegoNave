45a986d1592758f7a4d280bfbb0ffbd6c77aa69d
kennerm <47577403+GoldGlitch@users.noreply.github.com>
2021-11-22 23:31:46 +0100

    Actualización 22/11
    Asteroides creados, Explosiones añadidas, GameController integrado y generador de asteroides hecho

---
+++ Proyecto de naves/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        //Vector3 angularVelocity = new Vector3(Random.Range(-1,1), Random.Range(-1, 1), Random.Range(-1, 1)).normalized;
+        
+       rig.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+   
+}
