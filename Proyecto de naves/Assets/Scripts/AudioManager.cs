577a3c74162b8decd8c95f8cb1b92fd264468b9a
RyuChmepy <fernandochen123@gmail.com>
2021-12-13 23:22:34 +0100

    13/12
    Control de volumenes añadido
    Implementacion de opciones

---
+++ Proyecto de naves/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] AudioSource ShotSource;
+    [SerializeField] List<AudioClip> eatClip = new List<AudioClip>();
+
+    public const string MUSIC_KEY = "musicVolumen";
+    public const string SFX_KEY = "sfxVolumen";
+
+    private void Awake()
+    {
+           if ( instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        LoadVolume();
+    }
+
+    void LoadVolume()
+    {
+        float musicVolume = PlayerPrefs.GetFloat(MUSIC_KEY,1F);
+        float sfxVolume = PlayerPrefs.GetFloat(SFX_KEY, 1F);
+
+        mixer.SetFloat(Volume.MIXER_MUSIC,Mathf.Log10(musicVolume)* 20);
+        mixer.SetFloat(Volume.MIXER_SFX, Mathf.Log10(sfxVolume) * 20);
+    }
+    public void ShotSFX()
+    {
+        AudioClip clip = eatClip[Random.Range(0, eatClip.Count)];
+        ShotSource.PlayOneShot(clip);
+    }
+}
---
+++ Proyecto de naves/Assets/Scripts/Volume.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    [SerializeField] AudioMixer mixer;
+    [SerializeField] Slider musciSlider;
+    [SerializeField] Slider sfxSlider;
+
+    public const string MIXER_MUSIC = "MusicVolumen";
+    public const string MIXER_SFX = "SfxVolumen";
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(AudioManager.MUSIC_KEY,musciSlider.value);
+        PlayerPrefs.SetFloat(AudioManager.SFX_KEY, sfxSlider.value);
+    }
+
+
+    private void Start()
+    {
+        musciSlider.value = PlayerPrefs.GetFloat(AudioManager.MUSIC_KEY, 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat(AudioManager.SFX_KEY, 1f);
+    }
+    void Awake()
+    {
+        musciSlider.onValueChanged.AddListener(SetMusicVolumen);
+        sfxSlider.onValueChanged.AddListener(SetSFXVolumen);
+
+    }
+
+    void SetMusicVolumen(float value)
+    {
+        mixer.SetFloat(MIXER_MUSIC,Mathf.Log10(value)*20);
+
+    }
+    void SetSFXVolumen(float value)
+    {
+        mixer.SetFloat(MIXER_SFX, Mathf.Log10(value) * 20);
+
+    }
+}
