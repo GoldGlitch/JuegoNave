500c5aed90e9703ae058cfeb980f840bcb653c5a
RyuChmepy <fernandochen123@gmail.com>
2021-12-31 14:46:10 +0100

    31/12
    resoluciones arreglados

---
+++ Proyecto de naves/Assets/Scripts/LimitSceen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitSceen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateBoundary();
+    }
+    void UpdateBoundary()
+    {
+        Vector2 half = Utils.GetHalfDimensionsInWorldUnits() / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
